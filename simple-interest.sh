0d5ce511c4b82b44cbeecc02a17b9dacd1e9c4bc
Hidd3nMan <140346012+Hidd3nMan@users.noreply.github.com>
2023-07-26 21:59:15 -0400

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # <Hidd3nMan>
    # Input:
    # p, principal amount
    # t, time period in years
